cc5283b8a95a57159cbe344d01eabb1ef816c4d0
dgursoy <sparsedata@gmail.com>
2013-12-04 13:13:52 -0600

    Syntax updates.

--- dataio/data_read.py
+++ dataio/data_read.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
-# file_name: data_read.py
+# input_file: data_read.py
 from file_types import Hdf5
 
 class Dataset():
     def __init__(self):
         pass
 
-    def read_hdf5(self, file_name,
+    def read_hdf5(self, input_file,
                   projections_start=None,
                   projections_end=None,
                   projections_step=None,
@@ -16,15 +16,15 @@ class Dataset():
                   pixels_start=None,
                   pixels_end=None,
                   pixels_step=None,
-                  whiteStart=None,
-                  whiteEnd=None,
-                  darkStart=None,
-                  darkEnd=None):
+                  white_start=None,
+                  white_end=None,
+                  dark_start=None,
+                  dark_end=None):
         """ Read Data Exchange HDF5 file.
 
         Parameters
         ----------
-        file_name : str
+        input_file : str
             Input file.
 
         projections_start, projections_end, projections_step : scalar, optional
@@ -39,22 +39,22 @@ class Dataset():
             Values of the start, end and step of the pixels to
             be used for slicing for the whole ndarray.
 
-        whiteStart, whiteEnd : scalar, optional
+        white_start, white_end : scalar, optional
             Values of the start, end and step of the
             slicing for the whole white field shots.
 
-        darkStart, darkEnd : scalar, optional
+        dark_start, dark_end : scalar, optional
             Values of the start, end and step of the
             slicing for the whole dark field shots.
         """
         print "Reading data..."
-        self.file_name = file_name
+        self.input_file = input_file
 
         # Initialize f to null.
         f = None
 
-        # Get the file_name in lower case.
-        lFn = file_name.lower()
+        # Get the input_file in lower case.
+        lFn = input_file.lower()
 
         # Split the string with the delimeter '.'
         end = lFn.split('.')
@@ -68,7 +68,7 @@ class Dataset():
         # If f != None the call read on it.
         if not f == None:
             # Read data from exchange group.
-            self.data = f.read(file_name,
+            self.data = f.read(input_file,
                                 arrayName='exchange/data',
                                 projections_start=projections_start,
                                 projections_end=projections_end,
@@ -81,10 +81,10 @@ class Dataset():
                                 pixels_step=pixels_step)
 
             # Read white field data from exchange group.
-            self.white = f.read(file_name,
+            self.white = f.read(input_file,
                                 arrayName='exchange/data_white',
-                                projections_start=whiteStart,
-                                projections_end=whiteEnd,
+                                projections_start=white_start,
+                                projections_end=white_end,
                                 slices_start=slices_start,
                                 slices_end=slices_end,
                                 slices_step=slices_step,
@@ -93,10 +93,10 @@ class Dataset():
                                 pixels_step=pixels_step)
 
             # Read dark field data from exchange group.
-            self.dark = f.read(file_name,
+            self.dark = f.read(input_file,
                                 arrayName='exchange/data_dark',
-                                projections_start=darkStart,
-                                projections_end=darkEnd,
+                                projections_start=dark_start,
+                                projections_end=dark_end,
                                 slices_start=slices_start,
                                 slices_end=slices_end,
                                 slices_step=slices_step,
