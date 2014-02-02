27ee844d2c54cfb83f0faefa50ab17edf78a5f67
dgursoy <dgursoy@Dogas-MacBook-Pro.local>
2014-02-01 18:05:16 -0600

    diagnose center added

---
+++ tomopy/recon/diagnose_center.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import shutil
+import os
+from scipy.optimize import minimize
+from scipy import ndimage
+from scipy import misc
+from gridrec import Gridrec
+import logging
+logger = logging.getLogger("tomopy")
+
+
+def diagnose_center(data,
+                    theta,
+                    slice_no=None,
+                    center_start=None,
+                    center_end=None,
+                    center_step=None):
+    """ 
+    Diagnostic tools to find rotation center.
+    
+    Helps finding the rotation center manually by
+    visual inspection of the selected reconstructions
+    with different centers. The outputs for different
+    centers are put into ``data/diagnose`` directory
+    and the corresponding center positions are printed
+    so that one can skim through the images and
+    select the best.
+    
+    Parameters
+    ----------
+    data : ndarray
+        Input data.
+    
+    slice_no : scalar, optional
+        The index of the slice to be used for diagnostics.
+    
+    center_start, center_end, center_step : scalar, optional
+        Values of the start, end and step of the center values to
+        be used for diagnostics.
+    """
+    num_projections =  data.shape[0]
+    num_slices =  data.shape[1]
+    num_pixels =  data.shape[2]
+    
+    # Define diagnose region.
+    if slice_no is None:
+        slice_no = num_slices / 2
+    if center_start is None:
+        center_start = (num_pixels / 2) - 20
+    if center_end is None:
+        center_end = (num_pixels / 2) + 20
+    if center_step is None:
+        center_step = 1
+    center_step /= 2.
+
+    # Make preperations for the slices and corresponding centers.
+    slice_data = data[:, slice_no, :]
+    center = np.arange(center_start, center_end, center_step)
+    num_center = center.size
+    stacked_slices = np.zeros((num_projections, num_center, num_pixels))
+    for m in range(num_center):
+        stacked_slices[:, m, :] = slice_data
+
+    # Reconstruct the same slice with different centers.
+    recon = Gridrec(stacked_slices)
+    recon.run(stacked_slices, center=center, theta=theta)
+
+    # Save it to a temporary directory for manual inspection.
+    if os.path.isdir("tmp"):
+        shutil.rmtree("tmp")
+    os.makedirs("./tmp")
+    logger.debug("tmp directory create [ok]")
+    for m in range(center.size):
+        if m % 2 == 0: # 2 slices same bec of gridrec
+            img = misc.toimage(recon.data_recon[m, :, :])
+            file_name = "tmp/" + str(np.squeeze(center[m])) + ".tif"
+            img.save(file_name)
+    logger.debug("save diagnostic images [ok]")
+
+
+
+
+
+
+
--- tomopy/recon/recon.py
+++ tomopy/recon/recon.py
@@ -1,14 +1,22 @@
 # -*- coding: utf-8 -*-
 from tomopy.dataio.reader import Dataset
 from gridrec import Gridrec
+from diagnose_center import diagnose_center
 from optimize_center import optimize_center
 import logging
 logger = logging.getLogger("tomopy")
 
 
+def diagnose_center_wrapper(TomoObj, *args, **kwargs):
+    if TomoObj.FLAG_DATA and TomoObj.FLAG_THETA:
+        diagnose_center(TomoObj.data, TomoObj.theta, *args, **kwargs)
+        logger.info("diagnose rotation center [ok]")
+    else:
+        logger.warning("diagnose rotation center [bypassed]")
+
 def optimize_center_wrapper(TomoObj, *args, **kwargs):
-    if TomoObj.FLAG_DATA:
-        TomoObj.center = optimize_center(TomoObj.data, *args, **kwargs)
+    if TomoObj.FLAG_DATA and TomoObj.FLAG_THETA:
+        TomoObj.center = optimize_center(TomoObj.data, TomoObj.theta, *args, **kwargs)
         logger.info("optimize rotation center [ok]")
     else:
         logger.warning("optimize rotation center [bypassed]")
@@ -28,8 +36,10 @@ def gridrec_wrapper(TomoObj, *args, **kwargs):
         logger.warning("gridrec reconstruction [bypassed]")
 
 
+setattr(Dataset, 'diagnose_center', diagnose_center_wrapper)
 setattr(Dataset, 'optimize_center', optimize_center_wrapper)
 setattr(Dataset, 'gridrec', gridrec_wrapper)
 
+diagnose_center_wrapper.__doc__ = diagnose_center.__doc__
 optimize_center_wrapper.__doc__ = optimize_center.__doc__
 gridrec_wrapper.__doc__ = Gridrec.__doc__
