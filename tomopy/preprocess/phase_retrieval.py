9c085bf3e79bd4daeaf9fa1a1881408ae084f8b0
dgursoy <sparsedata@gmail.com>
2014-02-04 14:37:54 -0600

    phase retrieval test
    Former-commit-id: 91f7d7e4e4627b8ebc8213861863e533c75a3997

--- tomopy/preprocess/phase_retrieval.py
+++ tomopy/preprocess/phase_retrieval.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 # Filename: phase_retrieval.py
 import numpy as np
+from tomopy.tools import constants
+from tomopy.tools import fftw
 
 
-def phase_retrieval(data, pixel_size, dist, energy, alpha=1):
+def phase_retrieval(data, pixel_size, dist, energy, alpha=0.001, padding=True):
     """
     Perform single-material phase retrieval
     using projection data.
@@ -22,8 +24,12 @@ def phase_retrieval(data, pixel_size, dist, energy, alpha=1):
     energy : scalar
         Energy of x-rays in keV.
 
-    alpha : scalar
+    alpha : scalar, optional
         Regularization parameter.
+
+    padding : bool, optional
+        Applies padding for Fourier transform. For quick testing
+        you can use False for faster results.
 
     Returns
     -------
@@ -39,13 +45,16 @@ def phase_retrieval(data, pixel_size, dist, energy, alpha=1):
     wavelength = 2 * constants.PI * constants.PLANCK_CONSTANT * \
                  constants.SPEED_OF_LIGHT / energy
 
-    # Fourier padding in powers of 2.
-    pad_pixels = np.ceil(constants.PI * wavelength * dist / pixel_size ** 2)
-    num_x = pow(2, np.ceil(np.log2(dx + pad_pixels)))
-    num_y = pow(2, np.ceil(np.log2(dy + pad_pixels)))
-    y_shift = int((num_x - dx) / 2.0)
-    x_shift = int((num_y - dy) / 2.0)
-    tmp_data = np.ones((num_x, num_y), dtype='complex')
+    if padding:
+	# Fourier padding in powers of 2.
+	pad_pixels = np.ceil(constants.PI * wavelength * dist / pixel_size ** 2)
+	num_x = pow(2, np.ceil(np.log2(dx + pad_pixels)))
+	num_y = pow(2, np.ceil(np.log2(dy + pad_pixels)))
+	y_shift = int((num_x - dx) / 2.0)
+	x_shift = int((num_y - dy) / 2.0)
+	tmp_data = np.ones((num_x, num_y), dtype='complex')
+    elif not padding:
+        num_x, num_y = data.shape
 
     # Sampling in reciprocal space.
     indx = (1 / ((num_x-1) * pixel_size)) * np.arange(-(num_x-1)*0.5, num_x*0.5)
@@ -54,12 +63,18 @@ def phase_retrieval(data, pixel_size, dist, energy, alpha=1):
     w2 = np.square(du) + np.square(dv)
 
     # Filter in Fourier space.
-    H = 1 / (dist * wavelength * w2 / (4 * constants.PI)  + alpha)
+    H = 1 / (wavelength * dist * w2 / (4 * constants.PI) + alpha)
 
-    # Fourier transform of data.
-    tmp_data[y_shift:dx+y_shift, x_shift:dy+x_shift] = data
-    fft_data = np.fft.fftshift(np.fft.fft2(tmp_data))
-    filtered_data = np.fft.ifftshift(np.multiply(H, fft_data))
-    tmp = -np.real(np.fft.ifft2(filtered_data))
-    data = tmp[y_shift:dx+y_shift, x_shift:dy+x_shift]
-    return data
+    if padding:
+	# Fourier transform of data.
+	tmp_data[y_shift:dx+y_shift, x_shift:dy+x_shift] = data
+	fft_data = np.fft.fftshift(np.fft.fft2(tmp_data))
+	filtered_data = np.fft.ifftshift(np.multiply(H, fft_data))
+	tmp = np.real(np.fft.ifft2(filtered_data))
+	data = tmp[y_shift:dx+y_shift, x_shift:dy+x_shift]
+    elif not padding:
+	# Fourier transform of data.
+	fft_data = np.fft.fftshift(fftw.fftw2(data))
+	filtered_data = np.fft.ifftshift(np.multiply(H, fft_data))
+	data = np.real(fftw.ifftw2(filtered_data))
+    return alpha + np.log(data)
