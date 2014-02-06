0705eaa8b01eda597a6dce9e95a0eba3ef25cc2b
dgursoy <sparsedata@gmail.com>
2014-02-06 16:06:37 -0600

    setup for linux

--- setup.py
+++ setup.py
@@ -3,96 +3,100 @@
 import os
 import io
 
-VERSION = '0.0.1'
-
-# Import setuptools.
 from ez_setup import use_setuptools
 use_setuptools()
-from setuptools import setup, find_packages, Extension
+from setuptools import setup, Extension, find_packages
 
-# Read stuff in README and CHANGES.
-def read(*filenames, **kwargs):
-    encoding = kwargs.get('encoding', 'utf-8')
-    sep = kwargs.get('sep', '\n')
-    buf = []
-    for filename in filenames:
-        with io.open(filename, encoding=encoding) as f:
-            buf.append(f.read())
-    return sep.join(buf)
-long_description = read('README.md', 'CHANGES.txt')
-
-# Allow setup.py to be run from any path.
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+# Check Python packages.
+try:
+    import numpy
+except ImportError:
+    raise ImportError("tomopy requires numpy 1.8.0 (hint: pip install numpy)")
+try:
+    import scipy
+except ImportError:
+    raise ImportError("tomopy requires scipy 0.13.2 (hint: pip install numpy)")
+try:
+    import h5py
+except ImportError:
+    raise ImportError("tomopy requires h5py 2.2.1 (hint: pip install h5py)")
+try:
+    from scipy.misc import toimage
+except ImportError:
+    raise ImportError("tomopy requires pillow 2.3.0 (hint: pip install pillow)")
+try:
+    import pywt
+except ImportError:
+    raise ImportError("tomopy requires pywavelets 0.2.2 (hint: pip install pywavelets)")
 
 # Get shared library location from environment variables.
 try:
-    LIB_TOMOPY = [os.environ['LIB_TOMOPY']]
+    LD_LIBRARY_PATH = os.environ['LD_LIBRARY_PATH'].split(os.pathsep)
 except KeyError:
-    LIB_TOMOPY = ['/usr/local/lib, /usr/lib', '/local/lib']
+    raise KeyError("You need to set LD_LIBRARY_PATH")
+    
+try:
+    C_INCLUDE_PATH = os.environ['C_INCLUDE_PATH'].split(os.pathsep)
+    C_INCLUDE_PATH += {os.path.abspath('tomopy/c/gridrec/include')}
+    C_INCLUDE_PATH += {os.path.abspath('tomopy/c/fftw/include')}
+except KeyError:
+    raise KeyError("You need to set C_INCLUDE_PATH")
 
 # Create FFTW shared-libraries.
-ext_module_fftw = Extension(name='tomopy.lib.libfftw',
-                            sources=['tomopy/c/fftw/src/fftw.cpp'],
-                            include_dirs=['tomopy/c/fftw/include'],
-                            library_dirs=LIB_TOMOPY,
-                            extra_link_args=['-lfftw3f'])
+ext_fftw = Extension(name='tomopy.lib.libfftw',
+		     sources=['tomopy/c/fftw/src/fftw.cpp'],
+		     include_dirs=C_INCLUDE_PATH,
+		     library_dirs=LD_LIBRARY_PATH,
+		     extra_link_args=['-lfftw3f'])
 
 # Create Gridrec shared-libraries.
-ext_module_gridrec = Extension(name='tomopy.lib.libgridrec',
-                            sources=['tomopy/c/gridrec/src/filters.cpp',
-                                     'tomopy/c/gridrec/src/grid.cpp',
-                                     'tomopy/c/gridrec/src/MessageQueue.cpp',
-                                     'tomopy/c/gridrec/src/pswf.cpp',
-                                     'tomopy/c/gridrec/src/tomoRecon.cpp',
-                                     'tomopy/c/gridrec/src/tomoReconPy.cpp'],
-                            include_dirs=['tomopy/c/gridrec/include'],
-                            library_dirs=LIB_TOMOPY,
-                            extra_link_args=['-lfftw3f',
-                                             '-lboost_thread',
-                                             '-lboost_system',
-                                             '-lboost_date_time'])
+ext_gridrec = Extension(name='tomopy.lib.libgridrec',
+			sources=['tomopy/c/gridrec/src/filters.cpp',
+				 'tomopy/c/gridrec/src/grid.cpp',
+				 'tomopy/c/gridrec/src/MessageQueue.cpp',
+				 'tomopy/c/gridrec/src/pswf.cpp',
+				 'tomopy/c/gridrec/src/tomoRecon.cpp',
+				 'tomopy/c/gridrec/src/tomoReconPy.cpp'],
+			include_dirs=C_INCLUDE_PATH,
+			library_dirs=LD_LIBRARY_PATH,
+			extra_link_args=['-lfftw3f',
+					 '-lboost_thread',
+					 '-lboost_system',
+					 '-lboost_date_time'])
 
 # Main setup configuration.
 setup(
-      name = 'tomopy',
-      version = VERSION,
-      packages = find_packages(),
-      install_requires = ['h5py',
-                          'pyWavelets>=0.2',
-                          'scipy>=0.13',
-                          'numpy>=1.8'],
+      name='tomopy',
+      version='0.0.1',
 
-      package_data = {
-          # If any package contains *.txt or *.rst files, include them:
-          '': ['*.txt', '*.rst'],
-      },
+      packages = find_packages(),
+      include_package_data = True,
       
       # Specify C/C++ file paths. They will be compiled and put into tomopy.lib:
-      ext_modules = [ext_module_fftw, ext_module_gridrec],
+      ext_modules=[ext_fftw, ext_gridrec],
       
-      # metadata for upload to PyPI
-      author = 'Doga Gursoy',
-      author_email = 'dgursoy@aps.anl.gov',
-      description = 'Imaging toolbox',
-      long_description = long_description,
-      license = 'BSD',
-      keywords = 'tomography reconstruction imaging',
-      url = 'http://aps.anl.gov/tomopy',
-      download_url = 'http://github.com/dgursoy/tomopy-test',
-      platforms = 'x86, x86_64',
-      classifiers = [
-                     'Development Status :: 4 - Beta',
-                     'License :: OSI Approved :: BSD License',
-                     'Intended Audience :: Science/Research',
-                     'Intended Audience :: Education',
-                     'Intended Audience :: Developers',
-                     'Natural Language :: English',
-                     'Operating System :: OS Independent',
-                     'Programming Language :: Python',
-                     'Programming Language :: Python :: 2.6',
-                     'Programming Language :: Python :: 2.7',
-                     'Programming Language :: C',
-                     'Programming Language :: C++',
-                     ]
+      author='Doga Gursoy',
+      author_email='dgursoy@aps.anl.gov',
+
+      description='Imaging toolbox',
+      keywords=['tomography', 'reconstruction', 'imaging'],
+      url='http://aps.anl.gov/tomopy',
+      download_url='http://github.com/tomopy/tomopy',
+
+      license='BSD',
+      platforms='Any',
+
+      classifiers=['Development Status :: 4 - Beta',
+		   'License :: OSI Approved :: BSD License',
+		   'Intended Audience :: Science/Research',
+		   'Intended Audience :: Education',
+		   'Intended Audience :: Developers',
+		   'Natural Language :: English',
+		   'Operating System :: OS Independent',
+		   'Programming Language :: Python',
+		   'Programming Language :: Python :: 2.6',
+		   'Programming Language :: Python :: 2.7',
+		   'Programming Language :: C',
+		   'Programming Language :: C++']
       )
 
