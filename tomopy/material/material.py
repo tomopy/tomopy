6bf4e77cddca772273234bcb22a054c01f8cae23
dgursoy <dgursoy@Dogas-MacBook-Pro.local>
2014-02-06 21:43:41 -0600

    compound class added
    Former-commit-id: ccb0fc0cfdfae8c23cb26188aff8f21d9bfa0321

--- tomopy/material/material.py
+++ tomopy/material/material.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import h5py
+import re
 from tomopy.tools import constants
 import numpy as np
 from scipy import interpolate
@@ -56,8 +57,57 @@ class Compound():
     def __init__(self, compound, density):
         self.compound = compound
         self.density = density
-
+    
     def mass_ratio(self):
+        elem_name = {'Al':'aluminium',
+                     'Be':'beryllium',
+                     'Cd':'cadmium',
+                     'Ca':'calcium',
+                     'C':'carbon',
+                     'Cs':'cesium',
+                     'Cl':'chlorine',
+                     'F':'fluorine',
+                     'Au':'gold',
+                     'H':'hydrogen',
+                     'I':'iodine',
+                     'Fe':'iron',
+                     'Pb':'lead',
+                     'Mg':'magnesium',
+                     'N':'nitrogen',
+                     'O':'oxygen',
+                     'P':'phosphorus',
+                     'K':'potassium',
+                     'Si':'silicon',
+                     'Na':'sodium',
+                     'S':'sulphur',
+                     'Zn':'zinc'}
+        
+        all_matches = re.findall('[A-Z][a-z]?|[0-9]+', self.compound)
+        elems = re.findall('[A-Z][a-z]?', self.compound)
+        quant = re.findall('\d+', self.compound)
+        num_elems = len(elems)
+        count_elems = np.ones(num_elems)
+        
+        count = 0
+        num_counts = 0
+        for each in all_matches:
+            if re.match('\d+', each):
+                count_elems[count-1] = quant[num_counts]
+                num_counts += 1
+                count -= 1
+            count += 1
+        
+        _f = h5py.File('elements.h5', 'r')
+        _atomic_weight = np.zeros(num_elems)
+        for m in range(num_elems):
+            _atomic_weight[m] = np.squeeze(_f[elem_name[elems[m]]+'/atomic_weight'][:])
+        
+        mass_ratio = np.zeros(num_elems)
+        for m in range(num_elems):
+            mass_ratio[m] = _atomic_weight[m] * count_elems[m] / np.dot(_atomic_weight, count_elems)
+        return mass_ratio
+    
+    def atom_concentration(self, energy):
         pass
     
     def total_attenuation(self, energy):
