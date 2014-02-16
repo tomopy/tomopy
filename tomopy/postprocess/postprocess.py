bcda0a56b4260c14d634e018d9e7c6123d1f5d25
dgursoy <dgursoy@Dogas-MacBook-Pro.local>
2014-02-15 21:20:17 -0600

    threshold based segmentation added
    Former-commit-id: 53c31de5cf93a1adffda5165d5a09a31bd1f063c

--- tomopy/postprocess/postprocess.py
+++ tomopy/postprocess/postprocess.py
@@ -2,8 +2,9 @@
 import numpy as np
 from tomopy.dataio.reader import Dataset
 from region_segment import region_segment
+from threshold_segment import threshold_segment
 import multiprocessing as mp
-from tomopy.tools import multiprocess
+from tomopy.tools.multiprocess import distribute_jobs
 import logging
 logger = logging.getLogger("tomopy")
 
@@ -16,18 +17,18 @@ from skimage.filter import sobel
 def region_segment_wrapper(TomoObj, low, high,
                            num_cores=None, chunk_size=None):
     if not TomoObj.FLAG_DATA_RECON:
-        logger.warning("segmentation (data missing) [bypassed]")
+        logger.warning("region based segmentation (recon data missing) [bypassed]")
         return
+    
+    # Normalize data first.
+    data = TomoObj.data_recon - TomoObj.data_recon.min()
+    data /= data.max()
     
     # Distribute jobs.
     axis = 0 # Slice axis
     args = (low, high)
-    data = TomoObj.data_recon - np.min(TomoObj.data_recon)
-    data /= np.max(data)
-    
-    TomoObj.data_recon = multiprocess.distribute_jobs(data,
-                                 region_segment, args,
-                                 axis, num_cores, chunk_size)
+    TomoObj.data_recon = distribute_jobs(data, region_segment, args,
+                                         axis, num_cores, chunk_size)
 
     # Update provenance.
     TomoObj.provenance['region_segment'] = {'low':low, 'high':high}
@@ -35,6 +36,33 @@ def region_segment_wrapper(TomoObj, low, high,
     logger.info("region based segmentation [ok]")
 
 
+def threshold_segment_wrapper(TomoObj, cutoff=None,
+                           num_cores=None, chunk_size=None):
+    if not TomoObj.FLAG_DATA_RECON:
+        logger.warning("threshold based segmentation (recon data missing) [bypassed]")
+        return
+    
+    # Normalize data first.
+    data = TomoObj.data_recon - TomoObj.data_recon.min()
+    data /= data.max()
+
+    if cutoff == None:
+        cutoff = 0.5
+
+    # Distribute jobs.
+    axis = 0 # Slice axis
+    args = (cutoff)
+    TomoObj.data_recon = distribute_jobs(data, threshold_segment, args,
+                                         axis, num_cores, chunk_size)
+                                                      
+    # Update provenance.
+    TomoObj.provenance['threshold_segment'] = {'cutoff':cutoff}
+    
+    logger.info("threshold based segmentation [ok]")
+
+
 setattr(Dataset, 'region_segment', region_segment_wrapper)
+setattr(Dataset, 'threshold_segment', threshold_segment_wrapper)
 
 region_segment_wrapper.__doc__ = region_segment.__doc__
+threshold_segment_wrapper.__doc__ = threshold_segment.__doc__
