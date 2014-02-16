e2cd634b8c0dce77105c07238e1087f8c2b2d76f
dgursoy <dgursoy@Dogas-MacBook-Pro.local>
2014-02-15 22:35:02 -0600

    adaptive thresholding
    Former-commit-id: 23383ac13ab5893cf8a86ffd08de4de24f848375

--- tomopy/postprocess/postprocess.py
+++ tomopy/postprocess/postprocess.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 from tomopy.dataio.reader import Dataset
+from adaptive_segment import adaptive_segment
 from region_segment import region_segment
 from threshold_segment import threshold_segment
 import multiprocessing as mp
@@ -13,6 +14,32 @@ from skimage import morphology
 from skimage.filter import sobel
 
 
+def adaptive_segment_wrapper(TomoObj, block_size=None, offset=None,
+                              num_cores=None, chunk_size=None):
+    if not TomoObj.FLAG_DATA_RECON:
+        logger.warning("adaptive thresholding based segmentation (recon data missing) [bypassed]")
+        return
+    
+    # Normalize data first.
+    data = TomoObj.data_recon - TomoObj.data_recon.min()
+    data /= data.max()
+    
+    if block_size == None:
+        block_size = 256
+
+    if offset == None:
+        offset = 0
+
+    # Distribute jobs.
+    axis = 0 # Slice axis
+    args = (block_size, offset)
+    TomoObj.data_recon = distribute_jobs(data, adaptive_segment, args,
+                                         axis, num_cores, chunk_size)
+                                         
+    # Update provenance.
+    TomoObj.provenance['adaptive_segment'] = {'block_size':block_size, 'offset':offset}
+
+    logger.info("adaptive thresholding based segmentation [ok]")
 
 def region_segment_wrapper(TomoObj, low, high,
                            num_cores=None, chunk_size=None):
@@ -61,8 +88,10 @@ def threshold_segment_wrapper(TomoObj, cutoff=None,
     logger.info("threshold based segmentation [ok]")
 
 
+setattr(Dataset, 'adaptive_segment', adaptive_segment_wrapper)
 setattr(Dataset, 'region_segment', region_segment_wrapper)
 setattr(Dataset, 'threshold_segment', threshold_segment_wrapper)
 
+adaptive_segment_wrapper.__doc__ = adaptive_segment.__doc__
 region_segment_wrapper.__doc__ = region_segment.__doc__
 threshold_segment_wrapper.__doc__ = threshold_segment.__doc__
