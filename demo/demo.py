454e017a9078a99b8b45670472fc853023eb770d
dgursoy <dgursoy@Dogas-MacBook-Pro.local>
2014-02-16 08:56:22 -0600

    demo update
    Former-commit-id: a861d57d8231c32b7602b3885ba78683e745e0d8

--- demo/demo.py
+++ demo/demo.py
@@ -1,28 +1,28 @@
 # -*- coding: utf-8 -*-
 """
-This is a template script  for simple image processing tasks. 
-For more functionality please check the documentation.
-
-Have fun!
-"""
+    This is a template script  for simple image processing tasks.
+    For more functionality please check the documentation.
+    
+    Have fun!
+    """
 # Import the glorious package.
 import tomopy
 
 # Create a NULL dataset.
-# All scripts starts with this line. 
+# All scripts starts with this line.
 # Use ``log='DEBUG'`` argument for dbugging.
 d = tomopy.Dataset()
 
 # Import data from HDF file.
 # You can use slices_start, slices_end, etc. (see docs)
 # arguments to read specific slices without loading
-# whole data array into memory. 
+# whole data array into memory.
 d.read('demo/data.h5')
 
-# Normalize data. 
+# Normalize data.
 d.normalize()
 
-# Find center of rotation axis. 
+# Find center of rotation axis.
 # See also:``diagnose_center()'' function
 d.optimize_center()
 
@@ -30,20 +30,16 @@ d.optimize_center()
 # For large artifacts increase level argument.
 # Or you can simply use ``d.stripe_removal()``
 # to apply maximum available level value.
-# By default uses the maximum number of processors,
-# but you can change it using ``num_processors`` argument.
 d.stripe_removal(level=2, sigma=2)
 
-# Apply phase retireval. 
+# Apply phase retireval.
 # Units are in [cgs] and energy is in [keV].
 # You can change alpha value for desired output.
-# By default uses the maximum number of processors,
-# but you can change it using ``num_processors`` argument.
 d.phase_retrieval(pixel_size=1e-4, dist=70, energy=30, alpha=1e-5)
 
 # Image reconstruction using Gridrec.
 # For parameters see docs. If center is undefined
-# gridrec() uses optimize_center() first to find center of rotation axis.
+# ``gridrec`` uses ``optimize_center`` first to find center of rotation axis.
 d.gridrec()
 
 # Export data to file.
@@ -54,8 +50,7 @@ d.recon_to_tiff('demo/recon_')
 d.remove_bg()
 d.recon_to_tiff('demo/bg_removed_')
 
-# If the image is kind of binary, then you
-# can try thresholding based segmentation.
-# See also: ``region_segment(low=0.35, high=0.65)``
+# You can as well try an thresholding using Otsu's method.
+# This usually works well before background removal too.
 d.threshold_segment()
 d.recon_to_tiff('demo/segmented_')
