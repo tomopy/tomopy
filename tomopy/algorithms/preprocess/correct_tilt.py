74a3e1f29989dc77c2326c2924c3d5ce4ab13dce
dgursoy <sparsedata@gmail.com>
2014-03-27 19:14:15 -0500

    Update correct_tilt.py

--- tomopy/algorithms/preprocess/correct_tilt.py
+++ tomopy/algorithms/preprocess/correct_tilt.py
@@ -5,7 +5,43 @@ from scipy.ndimage.interpolation import rotate
 
 def correct_tilt(data, angle):
     """
-    Correct for tilt angle.
+    Correct for tilt of the raw projection data. 
+
+    Parameters
+    ----------
+    data : ndarray
+        3-D tomographic data with dimensions:
+        [projections, slices, pixels]
+
+    angle : scalar
+        angle of rotation applied to data, data_white and data_dark data. 
+
+    Returns
+    -------
+    data : ndarray
+        rotated data.
+        
+    Examples
+    --------
+    - Rotate by 0.5 deg:
+        
+        >>> import tomopy
+        >>> 
+        >>> # Load sinogram
+        >>> myfile = 'demo/data.h5'
+        >>> data, white, dark, theta = tomopy.xtomo_reader(myfile)
+        >>> 
+        >>> # Construct tomo object
+        >>> d = tomopy.xtomo_dataset(log='error')
+        >>> d.dataset(data, white, dark, theta)
+        >>> 
+        >>> # Perform rotation
+        >>> d.rotate(0.5)
+        >>> 
+        >>> # Save sinogram after rotation
+        >>> output_file='tmp/after_rotation_'
+        >>> tomopy.xtomo_writer(d.data, output_file)
+        >>> print "Images are succesfully saved at " + output_file + '...'
     """
     num_projections = data.shape[0]
     
