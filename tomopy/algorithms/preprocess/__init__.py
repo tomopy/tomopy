3b2b2a568db95de5a815344d0a3ee546ee8cca76
David Haberthür <email@davidhaberthuer.ch>
2014-12-15 15:06:07 +0100

    Fixed PEP 8 coding style violations (all but E501)
    `pep8 --ignore=E501 --exclude=conf.py,tomopy/algo* */*/*/*.py` returns
    no output any :smiley:
    I tried to be more prudent with whitespace changes now and did them all
    manually.
    
    TODO: Fix E501 errors.
    
    As part of http://24pullrequests.com
    
    
    Former-commit-id: 2fd87624900be3e8e28cbbcfc36e7a2ac43caa7a

--- tomopy/algorithms/preprocess/__init__.py
+++ tomopy/algorithms/preprocess/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Pre-processing module containes a set of methods for 
+Pre-processing module contains a set of methods for
 the pre-reconstruction phase of the data. The transformations
 applied in this module are data-to-data transformations
 and aims to prepare data for reconstruction.
