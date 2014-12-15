8069fff696d2234dea504c3cb55cd1879670a0cc
David Haberthür <email@davidhaberthuer.ch>
2014-12-15 15:57:18 +0100

    Fix (most) PEP8 coding style violations
    TODO: Fix E501 errors.
    
    As part of http://24pullrequests.com
    
    
    Former-commit-id: a325c07f154e9a0f17400df43dcaadf5e41ecd3e [formerly e3296618feb9bb09d58692c90c6ed40731b91747]
    Former-commit-id: 86247cb0e69b1a300a0ae1c0569ffd2b6f821643

--- tomopy/algorithms/postprocess/apply_mask.py
+++ tomopy/algorithms/postprocess/apply_mask.py
@@ -3,53 +3,54 @@ import numpy as np
 
 # --------------------------------------------------------------------
 
+
 def apply_mask(data, ratio):
     """
     Apply circular mask to reconstructed data.
-    
+
     Parameters
     ----------
     data : ndarray, float32
         3-D reconstructed data with dimensions:
         [slices, pixels, pixels]
-        
+
     ratio : scalar, int
         Ratio of the circular mask's diameter in pixels
-        to the number of reconstructed image pixels 
+        to the number of reconstructed image pixels
         (i.e., the dimension of the images).
-         
+
     Returns
     -------
     output : ndarray
         Masked data.
-        
+
     Examples
     --------
     - Apply mask after reconstruction:
-        
+
         >>> import tomopy
-        >>> 
+        >>>
         >>> # Load data
         >>> myfile = 'demo/data.h5'
         >>> data, white, dark, theta = tomopy.xtomo_reader(myfile,  slices_start=0, slices_end=1)
-        >>> 
+        >>>
         >>> # Construct tomo object
         >>> d = tomopy.xtomo_dataset(log='error')
         >>> d.dataset(data, white, dark, theta)
         >>> d.normalize()
-        >>> 
+        >>>
         >>> # Reconstruct data
         >>> d.center=661.5
         >>> d.gridrec()
-        >>> 
+        >>>
         >>> # Save reconstructed data before masking
         >>> output_file='tmp/before_masking_'
         >>> tomopy.xtomo_writer(d.data_recon, output_file)
         >>> print "Images are succesfully saved at " + output_file + '...'
-        >>> 
+        >>>
         >>> # Apply masking
         >>> d.apply_mask()
-        >>> 
+        >>>
         >>> # Save reconstructed data after masking
         >>> output_file='tmp/after_masking_'
         >>> tomopy.xtomo_writer(d.data_recon, output_file)
@@ -59,10 +60,10 @@ def apply_mask(data, ratio):
     num_pixels = data.shape[1]
 
     # Apply circular mask.
-    rad = num_pixels/2
+    rad = num_pixels / 2
     y, x = np.ogrid[-rad:rad, -rad:rad]
-    mask = x*x + y*y > ratio*ratio*rad*rad
+    mask = x * x + y * y > ratio * ratio * rad * rad
     for m in range(num_slices):
         data[m, mask] = 0
-        
+
     return data
--- tomopy/algorithms/postprocess/threshold_segment.py
+++ tomopy/algorithms/postprocess/threshold_segment.py
@@ -4,30 +4,31 @@ import tomopy.tools.multiprocess_shared as mp
 
 # --------------------------------------------------------------------
 
+
 def threshold_segment(args):
     """
     Applies threshold based on Otsu's method to reconstructed data.
-    
-    Otsu’s method calculates an “optimal” threshold 
-    (marked by a red line in the histogram below) by 
-    maximizing the variance between two classes of pixels, 
-    which are separated by the threshold. Equivalently, 
+
+    Otsu’s method calculates an “optimal” threshold
+    (marked by a red line in the histogram below) by
+    maximizing the variance between two classes of pixels,
+    which are separated by the threshold. Equivalently,
     this threshold minimizes the intra-class variance.
-    
+
     Parameters
     ----------
     data : ndarray, float32
         3-D reconstructed data with dimensions:
         [slices, pixels, pixels]
-        
+
     cutoff : scalar, int
         Manually selected threshold value.
-         
+
     Returns
     -------
     output : ndarray
         Thresholded data.
-        
+
     References
     ----------
     - `http://en.wikipedia.org/wiki/Otsu’s_method \
@@ -37,14 +38,14 @@ def threshold_segment(args):
     """
     # Arguments passed by multi-processing wrapper
     ind, dshape, inputs = args
-    
+
     # Function inputs
-    data = mp.tonumpyarray(mp.shared_arr, dshape) # shared-array
+    data = mp.tonumpyarray(mp.shared_arr, dshape)  # shared-array
     cutoff = inputs
-    
+
     for m in ind:
         img = data[m, :, :]
-        if cutoff == None:
+        if cutoff is None:
             cutoff = threshold_otsu(img)
         img = img > cutoff
         data[m, :, :] = img
