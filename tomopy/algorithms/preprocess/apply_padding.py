b553ec276b2acdd857f6cf3cefb036bb3dce18ea
David Haberthür <email@davidhaberthuer.ch>
2014-12-15 15:06:07 +0100

    Fixed PEP 8 coding style violations (all but E501)
    `pep8 --ignore=E501 --exclude=conf.py,tomopy/algo* */*/*/*.py` returns
    no output any :smiley:
    I tried to be more prudent with whitespace changes now and did them all
    manually.
    
    TODO: Fix E501 errors.
    
    As part of http://24pullrequests.com
    
    
    Former-commit-id: 3b2b2a568db95de5a815344d0a3ee546ee8cca76 [formerly 2fd87624900be3e8e28cbbcfc36e7a2ac43caa7a]
    Former-commit-id: 903e800229edab3c0d65cff1d2bf34c01e47204b

--- tomopy/algorithms/preprocess/apply_padding.py
+++ tomopy/algorithms/preprocess/apply_padding.py
@@ -11,50 +11,51 @@ libprep = ctypes.CDLL(os.path.abspath(libpath))
 
 # --------------------------------------------------------------------
 
+
 def apply_padding(data, num_pad, pad_val):
     """
     Applies padding to each projection data.
-    
+
     Parameters
     ----------
     data : ndarray, float32
         3-D tomographic data with dimensions:
         [projections, slices, pixels]
-        
+
     num_pad : scalar, int32
-        New dimension of the projections  
+        New dimension of the projections
         after padding.
-        
+
     pad_val : scalar, float32
         Pad value.
-         
+
     Returns
     -------
     output : ndarray
         Padded data.
-        
+
     Examples
     --------
     - Apply padding:
-        
+
         >>> import tomopy
-        >>> 
+        >>>
         >>> # Load data
         >>> myfile = 'demo/data.h5'
         >>> data, white, dark, theta = tomopy.xtomo_reader(myfile, slices_start=0, slices_end=1)
-        >>> 
+        >>>
         >>> # Save data before padding
         >>> output_file='tmp/before_pad_'
         >>> tomopy.xtomo_writer(data, output_file, axis=1)
         >>> print "Images are succesfully saved at " + output_file + '...'
-        >>> 
+        >>>
         >>> # Construct tomo object
         >>> d = tomopy.xtomo_dataset(log='error')
         >>> d.dataset(data, white, dark, theta)
-        >>> 
+        >>>
         >>> # Perform padding
         >>> d.apply_padding()
-        >>> 
+        >>>
         >>> # Save data after padding
         >>> output_file='tmp/after_pad_'
         >>> tomopy.xtomo_writer(d.data, output_file, axis=1)
@@ -63,16 +64,16 @@ def apply_padding(data, num_pad, pad_val):
     num_projections = np.array(data.shape[0], dtype='int32')
     num_slices = np.array(data.shape[1], dtype='int32')
     num_pixels = np.array(data.shape[2], dtype='int32')
-    
+
     if num_pad < num_pixels:
         return data
 
     # Call C function.
     c_float_p = ctypes.POINTER(ctypes.c_float)
-    
-    padded_data = pad_val * np.ones((num_projections, num_slices, num_pad), 
-                           dtype='float32')
-    
+
+    padded_data = pad_val * np.ones((num_projections, num_slices, num_pad),
+                                    dtype='float32')
+
     libprep.apply_padding.restype = ctypes.POINTER(ctypes.c_void_p)
     libprep.apply_padding(data.ctypes.data_as(c_float_p),
                           ctypes.c_int(num_projections),
