e3296618feb9bb09d58692c90c6ed40731b91747
David Haberthür <email@davidhaberthuer.ch>
2014-12-15 15:57:18 +0100

    Fix (most) PEP8 coding style violations
    TODO: Fix E501 errors.
    
    As part of http://24pullrequests.com

--- tomopy/algorithms/postprocess/__init__.py
+++ tomopy/algorithms/postprocess/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Post-processing module containes a set of methods for 
+Post-processing module containes a set of methods for
 the post-reconstruction phase of the data. The transformations
 applied in this package are image-to-image transformations.
 """
