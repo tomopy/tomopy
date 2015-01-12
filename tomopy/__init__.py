918ef5bfeea45b3d81d0c6cb25133d2a20f43284
David J. Vine <djvine@gmail.com>
2015-01-12 17:55:24 -0600

    Still working on adding support for X-ray fluroescence tomography.

--- tomopy/__init__.py
+++ tomopy/__init__.py
@@ -8,6 +8,8 @@ except:
 import sys
 import os
 sys.path.insert(0, os.path.dirname(__file__) + '/tools')
+os.environ['LD_LIBRARY_PATH'] = os.path.abspath(os.path.dirname(__file__)+'/lib')
+os.environ['C_INCLUDE_PATH'] = os.path.abspath(os.path.dirname(__file__)+'/lib')
 
 # ---------X-ray absorption tomography imports---------
 
@@ -22,3 +24,17 @@ from xtomo.xtomo_dataset import XTomoDataset as xtomo_dataset
 import xtomo.xtomo_preprocess
 import xtomo.xtomo_recon
 import xtomo.xtomo_postprocess
+
+# ---------X-ray fluorescence tomography imports---------
+
+# Reader/Writer functions for xftomo data.
+#from xtomo.xtomo_io import xtomo_reader
+#from xtomo.xtomo_io import xtomo_writer
+
+# Main xtomo object constructor.
+from xftomo.xftomo_dataset import XFTomoDataset as xftomo_dataset
+
+# Hooks to other functions.
+#import xtomo.xtomo_preprocess
+#import xtomo.xtomo_recon
+#import xtomo.xtomo_postprocess
