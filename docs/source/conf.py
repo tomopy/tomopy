a498bc0358c6fee7346b5db015465b41a1456de2
Doga Gursoy <sparsedata@gmail.com>
2015-03-13 20:37:15 -0500

    Update conf.py
    Former-commit-id: 41c626d051935a62fade8376628188ea74ada309 [formerly ac08356701bc2a57265f9758db293e00f3d313da]
    Former-commit-id: 9b0585e21b635163b4b72e9cfa3831650cdec50f

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return Mock()
 
-MOCK_MODULES = ['numpy', 'scipy', 'h5py', 'matplotlib']
+MOCK_MODULES = ['numpy', 'scipy', 'scipy.misc', 'h5py', 'matplotlib', 'matplotlib.pylab', 'scikit-image', 'skimage.io']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
