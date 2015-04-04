e90f5509c22bf397c3a3bade74ba64db4eb26ee8
dgursoy <sparsedata@gmail.com>
2015-04-04 00:34:25 -0500

    MNT: bib test
    Former-commit-id: 32b2ab2a28db1594ce7d117029db0b5310ce0762

--- doc/conf.py
+++ doc/conf.py
@@ -33,6 +33,8 @@ sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
 
+# otherwise, readthedocs.org uses their theme by default, so no need to specify it
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
@@ -43,9 +45,17 @@ sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.mathjax',
-    'sphinxcontrib.bibtex'
+    'sphinx.ext.mathjax'
 ]
+try:
+    import sphinxcontrib.bibtex
+    extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.mathjax',
+    'sphinxcontrib.bibtex']
+except ImportError:
+    pass
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -117,6 +127,14 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 html_theme = 'default'
+
+# on_rtd is whether we are on readthedocs.org
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
