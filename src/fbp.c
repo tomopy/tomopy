7e8a91bcdd4e340093efc50276c8d4c1e15721cf
dgursoy <sparsedata@gmail.com>
2015-04-20 14:52:01 -0500

    ENH: different center for each slice
    Former-commit-id: 088b8eff8fe878a56e5736be5c52e3282a4490c5 [formerly e546a8a7dc9308b71efa74c9b604dc59be3d039c]
    Former-commit-id: 13c03e806f2c07bee34334e8d9b81d2c1f770fd2

--- src/fbp.c
+++ src/fbp.c
@@ -46,7 +46,7 @@
 
 void 
 fbp(
-    float *data, int dx, int dy, int dz, float center, float *theta,
+    float *data, int dx, int dy, int dz, float *center, float *theta,
     float *recon, int ngridx, int ngridy)
 {
     float *gridx = (float *)malloc((ngridx+1)*sizeof(float));
@@ -74,15 +74,14 @@ fbp(
     float* simdata;
     int ind_data, ind_recon;
 
-
-    preprocessing(ngridx, ngridy, dz, center, 
-        &mov, gridx, gridy); // Outputs: mov, gridx, gridy
-
     simdata = (float *)calloc((dx*dy*dz), sizeof(float));
 
     // For each slice
     for (s=0; s<dy; s++) 
     {
+        preprocessing(ngridx, ngridy, dz, center[s], 
+            &mov, gridx, gridy); // Outputs: mov, gridx, gridy
+            
         // For each projection angle 
         for (p=0; p<dx; p++) 
         {
