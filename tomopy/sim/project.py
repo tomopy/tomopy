BUG: precision problem
Former-commit-id: 608a658702aecb3b7feabd37fed638dbbb369056 [formerly 68c7f30ee2d224242fd1ee80bd9dbfbd631108c7]
Former-commit-id: cd5be1665352cf6c74ff8ce828237bd74743c39c

tomopy/sim/project.py
@@ -140,16 +140,15 @@ def project(obj, theta, center=None, ncore=None, nchunk=None):
         3D tomographic data.
     """
     obj = dtype.as_float32(obj)
+    theta = dtype.as_float32(theta)
 
     # Estimate data dimensions.
     ox, oy, oz = obj.shape
-    dx = len(theta)
+    dx = theta.size
     dy = ox
     dz = np.ceil(np.sqrt(oy * oy + oz * oz)).astype('int')
     shape = dx, dy, dz
     tomo = np.zeros(shape, dtype='float32')
-
-    theta = dtype.as_float32(theta)
     center = get_center(shape, center)
 
     mproc.init_obj(obj)