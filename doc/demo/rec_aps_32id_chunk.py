2b5a8745ad9051652a015df7f104bfa29169292d
Francesco De Carlo <decarlof@gmail.com>
2015-07-23 15:51:44 -0500

    moved demo under doc
    Former-commit-id: 4f66c67f37297a685a6b63378388e48246340c47

---
+++ doc/demo/rec_aps_32id_chunk.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""
+TomoPy example to reconstruct a micro-CT data set in chuck of sinogrmas. 
+This function is for reconstructing large data set on limited memory computers.
+"""
+
+import tomopy 
+
+def rec_aps_32id_chunk():
+    """
+    
+    Warning
+    -------
+    Not implemented yet.
+    """
+    pass
+
+if __name__ == "__main__":
+    rec_aps_32id_chunk()
+
---
+++ doc/source/demo/doc.demo.rec_aps_32id_blocked_projs.rst
@@ -0,0 +1,9 @@
+Reconstruction with blocked views
+=================================
+
+.. literalinclude:: ../../../doc/demo/rec_aps_32id_blocked_projs.py
+	:tab-width: 4
+	:linenos:
+	:language: guess
+
+
