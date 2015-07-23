fa8cf3fb5241cd5eb62e58795be9e6131084e109
Francesco De Carlo <decarlof@gmail.com>
2015-07-23 16:48:12 -0500

    added examples for APS
    Former-commit-id: cc05fdc21d45577f7e5aece5eb8e54ee6f20a6de [formerly 57c2b62368f09dbe4d0782675a9177f257359f30]
    Former-commit-id: edbe8eb85ec29bcb3093fd79193a81a569547875

--- doc/demo/rec_aps_32id_full.py
+++ doc/demo/rec_aps_32id_full.py
@@ -6,44 +6,40 @@ TomoPy example to reconstruct a micro-CT data set.
 
 import tomopy 
 
-def rec_aps_32id_full():
+# set the path to the micro-CT data set ro reconstruct
+fname = 'data_dir/sample.h5'
 
-    # set the path to the micro-CT data set ro reconstruct
-    fname = 'data_dir/sample.h5'
+# Select the sinogram range to reconstruct
+start = 0; end = 2
 
-    # Select the sinogram range to reconstruct
-    start = 0; end = 2    
+# Read the APS 32-ID or 2-BM raw data
+prj, flat, dark = tomopy.io.exchange.read_aps_32id(fname, sino=(start, end))
 
-    # Read the APS 32-ID or 2-BM raw data
-    prj, flat, dark = tomopy.io.exchange.read_aps_32id(fname, sino=(start, end))
+# Set the data collection angles as equally spaced between 0-180 degrees
+theta  = tomopy.angles(prj.shape[0], ang1=0, ang2=180)
 
-    # Set the data collection angles as equally spaced between 0-180 degrees
-    theta  = tomopy.angles(prj.shape[0], ang1=0, ang2=180)
+# Normalize the raw projection data
+prj = tomopy.normalize(prj, flat, dark)
 
-    # Normalize the raw projection data
-    prj = tomopy.normalize(prj, flat, dark)
+# Set the aprox rotation axis location.
+# This parameter is the starting angle for auto centering routine
+start_center=295 
+print "Start Center: ", start_center
 
-    # Set the aprox rotation axis location.
-    # This parameter is the starting angle for auto centering routine
-    start_center=295 
-    print "Start Center: ", start_center
+# Auto centering
+calc_center = tomopy.find_center(prj, theta, emission=False, ind=0, init=start_center, tol=0.3)
+print "Calculated Center:", calc_center
 
-    # Auto centering
-    calc_center = tomopy.find_center(prj, theta, emission=False, ind=0, init=start_center, tol=0.3)
-    print "Calculated Center:", calc_center
+# Reconstruct using gridrec
+rec = tomopy.recon(prj, theta, center=calc_center, algorithm='gridrec', emission=False)
 
-    # recon using gridrec
-    rec = tomopy.recon(prj, theta, center=calc_center, algorithm='gridrec', emission=False)
+# Mask each reconstructed slice with a circle
+rec = tomopy.circ_mask(rec, axis=0, ratio=0.8)
 
-    # Mask each reconstructed slice with a circle
-    rec = tomopy.circ_mask(rec, axis=0, ratio=0.8)
+# Set reconstructed images name
+rec_name = 'rec/sample'
 
-    # to save the reconstructed images uncomment and customize the following line:
-    rec_name = 'rec/tooth'
+# Write data as stack of TIFs.
+tomopy.io.writer.write_tiff_stack(rec, fname=rec_name)
+print "Done!  reconstructions at: ", rec_name
 
-    # Write data as stack of TIFs.
-    tomopy.io.writer.write_tiff_stack(rec, fname=rec_name)
-    print "Done!  reconstructions at: ", rec_name
-
-if __name__ == "__main__":
-    rec_aps_32id_full()
