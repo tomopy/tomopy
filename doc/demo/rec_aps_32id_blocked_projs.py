0d6384d71748aea938adec31c1c591bfb4b61880
Doga Gursoy <sparsedata@gmail.com>
2015-07-23 21:06:57 -0500

    Update rec_aps_32id_blocked_projs.py

--- doc/demo/rec_aps_32id_blocked_projs.py
+++ doc/demo/rec_aps_32id_blocked_projs.py
@@ -1,59 +1,50 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 """
-TomoPy example to reconstruct a micro-CT data set containing a series 
-of dark projections becuase of the presence of an environment cell blocking
-some of the sample views.
+TomoPy example script to reconstruct a micro-CT data containing 
+a series of useless projections becuase of the presence of an 
+environment cell blocking some of the sample views.
 """
  
 import tomopy
 import numpy as np
 import matplotlib.pylab as pl
 
-# set the path to the micro-CT data set to reconstruct
+# Set path to the micro-CT data set to reconstruct.
 fname = 'data_dir/sample.h5'
 
-# Set the start/end index of the blocked projections 
-miss_angles = [141,226]
+# Set the [start, end] index of the blocked projections.
+miss_angles = [128, 256]
 
-# Select the sinogram range to reconstruct
-start = 740; end = 1700
+# Select sinogram range to reconstruct.
+start = 512
+end = 2048
 
-# Set the number of data chunks for the reconstruction
-chunks = 10 
-nSino_per_chunk = (end - start)/chunks
+# Set number of data chunks for the reconstruction.
+chunks = 64
+num_sino = (end - start) // chunks
 
-for iChunk in range(0,chunks):
-    print '\n  -- chunk # %i' % (iChunk+1)
-    sino_chunk_start = start + nSino_per_chunk*iChunk 
-    sino_chunk_end = start + nSino_per_chunk*(iChunk+1)
-    print '\n  --------> [%i, %i]' % (sino_chunk_start, sino_chunk_end)
+for m in range(chunks):
+    sino_start = start + num_sino * m 
+    sino_end = start + num_sino * (m + 1)
 
-    if sino_chunk_end > end: 
-        break
+    # Read APS 32-ID or 2-BM raw data.
+    proj, flat, dark = tomopy.read_aps_32id(fname, sino=(sino_start, sino_end))
 
-    # Read the APS 32-ID or 2-BM raw data
-    prj, flat, dark = tomopy.io.exchange.read_aps_32id(fname, sino=(sino_chunk_start, sino_chunk_end))
+    # Set data collection angles as equally spaced between 0-180 degrees.
+    theta  = tomopy.angles(prj.shape[0], 0, 180)
 
-    # Set the data collection angles as equally spaced between 0-180 degrees
-    theta  = tomopy.angles(prj.shape[0], ang1=0, ang2=180)
+    # Remove the missing angles from data.
+    proj = np.concatenate((proj[0:miss_angles[0], :, :], proj[miss_angles[1]+1:-1, :, :]), axis=0)
+    theta = np.concatenate((theta[0:miss_angles[0]], theta[miss_angles[1] + 1:-1]))
 
-    # Manage the missing angles
-    prj = np.concatenate((prj[0:miss_angles[0],:,:], prj[miss_angles[1]+1:-1,:,:]), axis=0)
-    theta = np.concatenate((theta[0:miss_angles[0]], theta[miss_angles[1]+1:-1]))
+    # Flat-field correction of raw data.
+    proj = tomopy.normalize(proj, flat, dark)
 
-    # Normalize the raw projection data
-    prj = tomopy.normalize(prj, flat, dark)
-
-    # Reconstruct using gridrec
-    best_center = 1232
-    rec = tomopy.recon(prj, theta, center=best_center, algorithm='gridrec', emission=False)
-
-    # Set reconstructed images name
-    rec_name = 'rec/sample'
-    print rec_name
+    # Reconstruct object using Gridrec algorithm.
+    rec = tomopy.recon(proj, theta, center=1024, algorithm='gridrec', emission=False)
 
     # Write data as stack of TIFs.
-    tomopy.io.writer.write_tiff_stack(rec, fname=rec_name, start=sino_chunk_start)
-
-
+    tomopy.io.writer.write_tiff_stack(rec, fname='recon_dir/recon', start=sino_start)
+    
