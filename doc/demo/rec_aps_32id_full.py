72e850ec7933d69b59571de13dd5d3fc4ee02fd1
Doga Gursoy <sparsedata@gmail.com>
2015-07-23 21:00:19 -0500

    Update rec_aps_32id_full.py
    Former-commit-id: 84cf029d99501c5f1c2d16ea45d2b93abcfe50f6

--- doc/demo/rec_aps_32id_full.py
+++ doc/demo/rec_aps_32id_full.py
@@ -12,7 +12,7 @@ fname = 'data_dir/sample.h5'
 
 # Select sinogram range to reconstruct.
 start = 0
-end = 2
+end = 16
 
 # Read APS 32-ID or 2-BM raw data.
 proj, flat, dark = tomopy.io.exchange.read_aps_32id(fname, sino=(start, end))
@@ -24,7 +24,7 @@ theta  = tomopy.angles(proj.shape[0], 0, 180)
 prj = tomopy.normalize(proj, flat, dark)
 
 # Find rotation center.
-rot_center = tomopy.find_center(proj, theta, emission=False, ind=0, init=295, tol=0.5)
+rot_center = tomopy.find_center(proj, theta, emission=False, ind=0, init=1024, tol=0.5)
 print "Calculated rotation center: ", rot_center
 
 # Reconstruct object using Gridrec algorithm.
@@ -33,7 +33,7 @@ rec = tomopy.recon(proj, theta, center=rot_center, algorithm='gridrec', emission
 # Mask each reconstructed slice with a circle.
 rec = tomopy.circ_mask(rec, axis=0, ratio=0.95)
 
-# Set reconstructed images name
+# Set reconstructed images name.
 rec_name = 'recon_dir/recon'
 
 # Write data as stack of TIFs.
