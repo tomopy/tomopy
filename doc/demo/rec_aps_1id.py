6a3b8d3c18b09202d24eed3787b1d29f4108aee8
dgursoy <sparsedata@gmail.com>
2015-08-18 09:32:38 -0500

    print function fix continues...
    Former-commit-id: 750142cfbe45c1f00cb0618eaed5481896e96440 [formerly 5fded37688e8d4a22bada75792c242c08883fe77]
    Former-commit-id: e6264cc996f80f6152a3ca34c9835679efab6a2c

--- doc/demo/rec_aps_1id.py
+++ doc/demo/rec_aps_1id.py
@@ -5,8 +5,8 @@
 TomoPy example script to reconstruct the APS 1-ID tomography data as original tiff.
 """
 
-import tomopy
 from __future__ import print_function
+import tomopy
 
 if __name__ == '__main__':
 
