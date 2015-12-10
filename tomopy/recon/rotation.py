e8217d9022104957102238fb9c81e7025cb1b00f
dgursoy <sparsedata@gmail.com>
2015-12-10 10:46:28 -0500

    FIX: back to the original version

--- tomopy/recon/rotation.py
+++ tomopy/recon/rotation.py
@@ -225,14 +225,10 @@ def find_center_vo(tomo, ind=None, smin=-40, smax=40, srad=10, step=1,
     _tomo = ndimage.filters.gaussian_filter(_tomo, sigma=(3, 1))
 
     # Coarse search for finding the rotation center.
-    if _tomo.shape[0] * _tomo.shape[1] > 4e6:  # If data is large (>2kx2k)
-        _tomo_coarse = downsample(tomo, level=2)[:, ind, :]
-        init_cen = _search_coarse(_tomo_coarse, smin, smax, ratio, drop)
-    else:
-        init_cen = _search_coarse(_tomo, smin, smax, ratio, drop)
+    init_cen = _search_coarse(_tomo, smin, smax, ratio, drop)
 
     # Fine search for finding the rotation center.
-    fine_cen = _search_fine(_tomo, srad, step, init_cen*4, ratio, drop)
+    fine_cen = _search_fine(_tomo, srad, step, init_cen, ratio, drop)
     logger.debug('Rotation center search finished: %i', fine_cen)
     return fine_cen
 
