3dd084b9821d2cec5b635886248e7891ff4cd72f
Francesco De Carlo <decarlof@gmail.com>
2016-08-30 12:39:50 -0500

    added concurrent mock

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -324,7 +324,7 @@ MOCK_MODULES = [
     'matplotlib', 'matplotlib.pylab', 'tifffile', 'EdfFile', 'netCDF4',
     'spefile', 'scipy.ndimage', 'pywt', 'scikit-image', 'skimage',
     'skimage.io', 'skimage.filter', 'skimage.morphology', 'skimage.feature',
-    'DM3lib', 'pyfftw', 'dxchange', 'numexpr', 'concurrent.futures']
+    'DM3lib', 'pyfftw', 'dxchange', 'numexpr', 'concurrent', 'concurrent.futures']
 
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
