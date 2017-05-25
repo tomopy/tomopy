c373c3b9c559efffb8b17b296df7683809407d99
Eric Yen <eyen@theobjects.com>
2017-05-24 22:58:12 -0400

    Bug fxies for windows

--- tomopy/misc/corr.py
+++ tomopy/misc/corr.py
@@ -386,7 +386,7 @@ def remove_outlier(arr, dif, size=3, axis=0, ncore=None, out=None):
 
     with cf.ThreadPoolExecutor(ncore) as e:
         slc = [slice(None)]*arr.ndim
-        for i in range(ncore):
+        for i in range(len(slc)):
             slc[axis] = chnk_slices[i]
             e.submit(filters.median_filter, arr[slc], size=filt_size,
                      output=tmp[slc])
@@ -437,7 +437,7 @@ def remove_outlier1d(arr, dif, size=3, axis=0, ncore=None, out=None):
     
     with cf.ThreadPoolExecutor(ncore) as e:
         slc = [slice(None)]*arr.ndim
-        for i in range(ncore):
+        for i in range(len(slc)):
             slc[lar_axis] = chnk_slices[i]
             e.submit(filters.median_filter, arr[slc], size=filt_size,
                      output=tmp[slc], mode='mirror')
