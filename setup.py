447dbc67be43411ecd39dc393da1c3f9418210c1
dgursoy <sparsedata@gmail.com>
2017-10-11 13:50:34 -0500

    MNT: setup update
    Former-commit-id: ed6d4567cfe120ec1808ba9c8b8208b8ffec1f8d [formerly 3ec2efb1ba5267fc34d5848a63f9273d86b4b430]
    Former-commit-id: 3a3097547b9a1ed31c61d432a1e6d1c97fdfd59d

--- setup.py
+++ setup.py
@@ -82,9 +82,10 @@ setup(
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: C']
 )
