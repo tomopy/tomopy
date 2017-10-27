7d642467cb7f4a56f8cbd9c5a3b0f5375342711c
Oleksandr Pavlyk <oleksandr.pavlyk@intel.com>
2017-10-27 15:25:36 -0500

    ENH: Modularized uses of fft/ifft/fft2/ifft2 into tomopy.util.misc
    Provided implementations of these function using mkl_fft or pyfftw or numpy.fft
    
    Implemented support for in-place FFT operation.
    
    Optimized constructon of _reciprocal_grid and _reciprocal_ccord
    
    
    Former-commit-id: 5cb31f693288c485363a94ed87744326bdbd4de4 [formerly 4f492a1134ff5c4ad697448de6dcf20aae10c8c3]
    Former-commit-id: 2e7595aad7ded3e448fe9110cf176b78b7f925f9

--- tomopy/util/misc.py
+++ tomopy/util/misc.py
@@ -56,13 +56,15 @@ from __future__ import (absolute_import, division, print_function,
 import logging
 import warnings
 
+
+
 logger = logging.getLogger(__name__)
 
 
 __author__ = "Doga Gursoy"
 __copyright__ = "Copyright (c) 2016, UChicago Argonne, LLC."
 __docformat__ = 'restructuredtext en'
-__all__ = ['deprecated']
+__all__ = ['deprecated', 'fft2', 'ifft2', 'fft', 'ifft']
 
 
 def deprecated(func, msg=None):
@@ -81,3 +83,83 @@ def deprecated(func, msg=None):
     new_func.__doc__ = func.__doc__
     new_func.__dict__.update(func.__dict__)
     return new_func
+
+
+try:
+    import mkl_fft
+    fft_impl = 'mkl_fft'
+    logger.debug('FFT implementation is mkl_fft')
+except ImportError:
+    try:
+        import pyfftw
+        fft_impl = 'pyfftw'
+        logger.debug('FFT implementation is pyfftw')
+    except ImportError:
+        import np.fft
+        fft_impl = 'numpy.fft'
+        logger.debug('FFT implementation is numpy.fft')
+
+
+if fft_impl == 'mkl_fft':
+    def fft(x, n=None, axis=-1, overwrite_input=False, extra_info=None):
+        return mkl_fft.fft(x, n=n, axis=axis, overwrite_x=overwrite_input)
+
+
+    def ifft(x, n=None, axis=-1, overwrite_input=False, extra_info=None):
+        return mkl_fft.ifft(x, n=n, axis=axis, overwrite_x=overwrite_input)
+
+
+    def fft2(x, s=None, axes=(-2,-1), overwrite_input=False, extra_info=None):
+        return mkl_fft.fft2(x, shape=s, axes=axes, overwrite_x=overwrite_input)
+
+
+    def ifft2(x, s=None, axes=(-2,-1), overwrite_input=False, extra_info=None):
+        return mkl_fft.ifft2(x, shape=s, axes=axes, overwrite_x=overwrite_input)
+
+elif fft_impl == 'pyfftw':
+    def _plan_effort(num_jobs):
+        if not num_jobs:
+            return 'FFTW_MEASURE'
+        if num_jobs > 10:
+            return 'FFTW_MEASURE'
+        else:
+            return 'FFTW_ESTIMATE'
+
+    def fft(x, n=None, axis=-1, overwrite_input=False, extra_info=None):
+        return pyfftw.interfaces.numpy_fft.fft(x, n=n, axis=axis,
+                                               overwrite_input=overwrite_input,
+                                               planner_effort=_plan_effort(extra_info))
+    
+
+    def ifft(x, n=None, axis=-1, overwrite_input=False, extra_info=None):
+        return pyfftw.interfaces.numpy_fft.ifft(x, n=n, axis=axis,
+                                                overwrite_input=overwrite_input,
+                                                planner_effort=_plan_effort(extra_info))
+
+
+    def fft2(x, s=None, axes=(-2,-1), overwrite_input=False, extra_info=None):
+        return pyfftw.interfaces.numpy_fft.fft2(x, s=s, axes=axes,
+                                                overwrite_input=overwrite_input,
+                                                planner_effort=_plan_effort(extra_info))
+    
+
+    def ifft2(x, s=None, axes=(-2,-1), overwrite_input=False, extra_info=None):
+        return pyfftw.interfaces.numpy_fft.ifft2(x, s=s, axes=axes,
+                                                 overwrite_input=overwrite_input,
+                                                 planner_effort=_plan_effort(extra_info))
+
+else:
+    def fft(x, n=None, axis=-1, overwrite_input=False, extra_info=None):
+        return np.fft.fft(x, n=n, axis=axis)
+
+
+    def ifft(x, n=None, axis=-1, overwrite_input=False, extra_info=None):
+        return np.fft.ifft(x, n=n, axis=axis)
+
+
+    def fft2(x, s=None, axes=(-2,-1), overwrite_input=False, extra_info=None):
+        return np.fft.fft2(x, s=s, axes=axes)
+
+
+    def ifft2(x, s=None, axes=(-2,-1), overwrite_input=False, extra_info=None):
+        return np.fft.ifft2(x, s=s, axes=axes)
