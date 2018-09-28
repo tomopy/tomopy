f6b09af355055b3c0747c809b4ff9bfe1852a23a
Daniel Ching <carterbox@users.noreply.github.com>
2018-09-28 18:48:20 -0500

    PEP8: Imports go at the top

--- tomopy/__init__.py
+++ tomopy/__init__.py
@@ -52,10 +52,6 @@ from __future__ import (absolute_import, division, print_function,
 __version__ = '1.1.4'
 
 from ._fft_loader import fft_impl
-
-import logging
-logging.getLogger(__name__).addHandler(logging.NullHandler())
-
 from tomopy.misc.corr import *
 from tomopy.misc.morph import *
 from tomopy.misc.phantom import *
@@ -71,3 +67,6 @@ from tomopy.recon.acceleration import *
 from tomopy.sim.project import *
 from tomopy.sim.propagate import *
 from tomopy.util.mproc import set_debug
+
+import logging
+logging.getLogger(__name__).addHandler(logging.NullHandler())
