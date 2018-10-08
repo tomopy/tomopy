94e58f8f735eb53f98007a5f9697342efc67405c
Daniel Ching <carterbox@users.noreply.github.com>
2018-10-08 15:17:36 -0500

    MAI: Update version to 1.2.1

--- tomopy/__init__.py
+++ tomopy/__init__.py
@@ -49,7 +49,7 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
-__version__ = '1.2.0'
+__version__ = '1.2.1'
 
 from ._fft_loader import fft_impl
 from tomopy.misc.corr import *
