bb8766248152a51ca93dbb07cd8c429f21738993
Chen <zdms1987@hotmail.com>
2018-11-26 13:50:08 -0600

    tighted the threshold slightly

--- tomopy/prep/alignment.py
+++ tomopy/prep/alignment.py
@@ -741,12 +741,12 @@ def detector_drift_adjust_aps_1id(imgstacks,
             cnr  = proj_cnrs[n_img,:,:]  # previous results
             # NOTE:
             #  The detector corner should not be far away from reference
-            #  >> adiff < 20
+            #  >> adiff < 10
             #  The detected corner should be stable
-            #  >> rdiff < 1 (pixel)s
+            #  >> rdiff < 0.1 (pixel)s
             adiff = quickDiff(_cnr - slit_cnr_ref)
             rdiff = quickDiff(_cnr - cnr) 
-            if rdiff < 1 and adiff < 20:
+            if rdiff < 0.1 and adiff < 10:
                 cnrs_found[n_img] = True
             else:
                 _tmpar = np.zeros((4, 5))
