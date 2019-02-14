1e3811820b86524f59b131b775e0a95b77e0c9a8
Jonathan R. Madsen <jonathanrmadsen@gmail.com>
2019-02-13 20:13:14 -0800

    Iteration info for C + ART

--- source/c/art.c
+++ source/c/art.c
@@ -160,6 +160,7 @@ art(const float* data, int dy, int dt, int dx, const float* center, const float*
                 }
             }
         }
+        printf("[c]> iteration %3i of %3i...\n", i, num_iter);
     }
     free(gridx);
     free(gridy);
