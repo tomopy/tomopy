db2ed593bdf4ba991143ff31ece5893fc5608632
Chen <zdms1987@hotmail.com>
2019-02-15 15:34:29 -0600

    add selective median filter

--- tomopy/misc/corr.py
+++ tomopy/misc/corr.py
@@ -749,3 +749,30 @@ def _calc_histequal_wgt(img):
         image
     """
     return (np.sort(img.flatten()).searchsorted(img) + 1)/np.prod(img.shape)
+
+
+def selctive_median_filter(img, threshold=None, kernel_size=3):
+    """
+    Use selective median filtering to remove impulse noises
+
+    Parameters
+    ----------
+    img : ndarray
+        2D images
+    threshold: float
+        Expected difference between impulse noise and the meidan
+        value, default is 5% of the median value of the image
+    kernel_size: int
+        Kernel size of the median filter
+
+    Returns
+    -------
+    ndarray
+        Image with impulse noises replaced with the meidan values.
+    """
+    threshold = 0.05*np.median(img) if threshold is None else threshold
+
+    _img_median = medfilt2d(img, kernel_size=kernel_size)
+
+    return np.where(np.absolute(img - _img_median) < threshold, img, _img_median)
+
