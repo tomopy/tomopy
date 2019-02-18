8ed38e7a00af38745bb5824fab438ce293226fbc
Jonathan R. Madsen <jonathanrmadsen@gmail.com>
2019-02-18 14:38:21 -0800

    SLURM updates

---
+++ slurm/env-common-settings.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+module load python/3.6-anaconda-4.4
+module load gcc
+module load cuda
+source activate tomopy-gpu
+
+# algorithm settings
+: ${TOMOPY_INTER:=1}
+: ${TOMOPY_NUM_ITERATION:=50}
+: ${DATA_TYPE:=partial}
+
+if [ -n "${1}" ]; then TOMOPY_INTER=${1}; shift; fi
+if [ -n "${1}" ]; then TOMOPY_NUM_ITERATION=${1}; shift; fi
+
+# data settings
+: ${BLOCK_BEG:=0}
+: ${BLOCK_END:=24}
+: ${NUM_SLICES:=$((${BLOCK_END} - ${BLOCK_BEG}))}
+
+# parallelism settings
+: ${PTL_CPU_AFFINITY:=1}
+: ${PTL_VERBOSE:=1}
+: ${TOMOPY_NUM_GPU:=8}
+: ${TOMOPY_PYTHON_THREADS:=${TOMOPY_NUM_GPU}}
+: ${TOMOPY_NUM_THREADS:=$(( $(nproc) / ${TOMOPY_PYTHON_THREADS} + 2 ))}
+: ${CUDA_BLOCK_SIZE:=32}
+: ${CUDA_GRID_SIZE:=0}
+
+PTL_NUM_THREADS=${TOMOPY_NUM_THREADS}
+NUMEXPR_MAX_THREADS=$(nproc)
+
+export TOMOPY_INTER TOMOPY_NUM_ITERATION DATA_TYPE
+export BLOCK_BEG BLOCK_END NUM_SLICES
+export PTL_CPU_AFFINITY PTL_VERBOSE PTL_NUM_THREADS NUMEXPR_MAX_THREADS
+export TOMOPY_NUM_GPU TOMOPY_PYTHON_THREADS TOMOPY_NUM_THREADS
+export CUDA_BLOCK_SIZE CUDA_GRID_SIZE
+
+configure-out()
+{
+    echo "${TOMOPY_DEVICE_TYPE}-${DATA_TYPE}-${TOMOPY_NUM_GPU}-${TOMOPY_INTER}-${CUDA_BLOCK_SIZE}-${CUDA_GRID_SIZE}-${TOMOPY_PYTHON_THREADS}-${TOMOPY_NUM_THREADS}-${TOMOPY_USE_C_ALGORITHMS}-${TOMOPY_USE_CPU}"
+}
+
+run-verbose()
+{
+    echo -e "\nRunning : '$@'...\n"
+    eval $@
+}
