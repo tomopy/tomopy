f0aacc03c2487b62cbe913961c26488bf69087c4
Daniel Ching <carterbox@users.noreply.github.com>
2020-01-18 15:56:55 -0600

    DOC: Add sphinx >=2 requirement
    The sphinx bibtex extension now requires
    sphinx >=2.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -24,7 +24,7 @@ sys.path.insert(0, os.path.abspath('../../source'))
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-#needs_sphinx = '1.0'
+needs_sphinx = '2.0'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
