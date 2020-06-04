3a960aceb80758a4adf6248585ec971c9564dfb5
dreycenfoiles <foilesdreycen@gmail.com>
2020-06-04 15:38:59 -0500

    Moved gridrec to its own library and changed CMake instructions to only build one version, which is dependent on the detected OS

--- source/tomopy/util/extern.py
+++ source/tomopy/util/extern.py
@@ -119,11 +119,12 @@ with warnings.catch_warnings():
 	warnings.simplefilter("ignore")
 	LIB_PTL = c_shared_lib('libptl', do_warn=False)
 
-LIB_TOMOPY = c_shared_lib('libtomopy')
+# LIB_TOMOPY = c_shared_lib('libtomopy')
 LIB_TOMOPY_MISC = c_shared_lib("libtomopy-misc")
 LIB_TOMOPY_PREP = c_shared_lib("libtomopy-prep")
 LIB_TOMOPY_RECON = c_shared_lib("libtomopy-recon")
 LIB_TOMOPY_ACCEL = c_shared_lib("libtomopy-accel")
+LIB_TOMOPY_GRIDREC = c_shared_lib("libtomopy-gridrec")
 
 
 def c_normalize_bg(tomo, air):
@@ -356,8 +357,8 @@ def c_gridrec(tomo, center, recon, theta, **kwargs):
 	else:
 		dy, dt, dx = tomo.shape
 
-	LIB_TOMOPY.gridrec.restype = dtype.as_c_void_p()
-	return LIB_TOMOPY.gridrec(
+	LIB_TOMOPY_GRIDREC.gridrec.restype = dtype.as_c_void_p()
+	return LIB_TOMOPY_GRIDREC.gridrec(
 		dtype.as_c_float_p(tomo),
 		dtype.as_c_int(dy),
 		dtype.as_c_int(dt),
