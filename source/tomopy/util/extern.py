471ad620a9933ba534b9c460d90d50164042038f
dreycenfoiles <foilesdreycen@gmail.com>
2020-06-10 16:54:12 -0500

    Fixed mismatched arguments in accelerated reconstruction algorithms

--- source/tomopy/util/extern.py
+++ source/tomopy/util/extern.py
@@ -392,7 +392,12 @@ def c_mlem(tomo, center, recon, theta, **kwargs):
             dtype.as_c_float_p(recon),
             dtype.as_c_int(kwargs['num_gridx']),
             dtype.as_c_int(kwargs['num_gridy']),
-            dtype.as_c_int(kwargs['num_iter']))
+            dtype.as_c_int(kwargs['num_iter']),
+            dtype.as_c_int(kwargs['pool_size']),
+            dtype.as_c_char_p(kwargs['interpolation']),
+            dtype.as_c_char_p(kwargs['device']),
+            dtype.as_c_int_p(kwargs['grid_size']),
+            dtype.as_c_int_p(kwargs['block_size']))
     else:
         LIB_TOMOPY_RECON.mlem.restype = dtype.as_c_void_p()
         return LIB_TOMOPY_RECON.mlem(
@@ -405,12 +410,7 @@ def c_mlem(tomo, center, recon, theta, **kwargs):
             dtype.as_c_float_p(recon),
             dtype.as_c_int(kwargs['num_gridx']),
             dtype.as_c_int(kwargs['num_gridy']),
-            dtype.as_c_int(kwargs['num_iter']),
-            dtype.as_c_int(kwargs['pool_size']),
-            dtype.as_c_char_p(kwargs['interpolation']),
-            dtype.as_c_char_p(kwargs['device']),
-            dtype.as_c_int_p(kwargs['grid_size']),
-            dtype.as_c_int_p(kwargs['block_size']))
+            dtype.as_c_int(kwargs['num_iter']))
 
 
 def c_osem(tomo, center, recon, theta, **kwargs):
@@ -556,7 +556,12 @@ def c_sirt(tomo, center, recon, theta, **kwargs):
             dtype.as_c_float_p(recon),
             dtype.as_c_int(kwargs['num_gridx']),
             dtype.as_c_int(kwargs['num_gridy']),
-            dtype.as_c_int(kwargs['num_iter']))
+            dtype.as_c_int(kwargs['num_iter']),
+            dtype.as_c_int(kwargs['pool_size']),
+            dtype.as_c_char_p(kwargs['interpolation']),
+            dtype.as_c_char_p(kwargs['device']),
+            dtype.as_c_int_p(kwargs['grid_size']),
+            dtype.as_c_int_p(kwargs['block_size']))
     else:
         LIB_TOMOPY_RECON.sirt.restype = dtype.as_c_void_p()
         return LIB_TOMOPY_RECON.sirt(
@@ -569,12 +574,7 @@ def c_sirt(tomo, center, recon, theta, **kwargs):
             dtype.as_c_float_p(recon),
             dtype.as_c_int(kwargs['num_gridx']),
             dtype.as_c_int(kwargs['num_gridy']),
-            dtype.as_c_int(kwargs['num_iter']),
-            dtype.as_c_int(kwargs['pool_size']),
-            dtype.as_c_char_p(kwargs['interpolation']),
-            dtype.as_c_char_p(kwargs['device']),
-            dtype.as_c_int_p(kwargs['grid_size']),
-            dtype.as_c_int_p(kwargs['block_size']))
+            dtype.as_c_int(kwargs['num_iter']))
 
 
 def c_tv(tomo, center, recon, theta, **kwargs):
