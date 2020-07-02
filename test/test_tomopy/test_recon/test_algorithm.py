0bbb511d0b19861403344ac079cc474c70e70764
dreycenfoiles <foilesdreycen@gmail.com>
2020-07-02 12:54:11 -0500

    Fixed accel testing

--- test/test_tomopy/test_recon/test_algorithm.py
+++ test/test_tomopy/test_recon/test_algorithm.py
@@ -122,7 +122,7 @@ class ReconstructionAlgorithmTestCase(unittest.TestCase):
 
     def test_mlem_accel(self):
         result = recon(self.prj, self.ang, algorithm='mlem', num_iter=4,
-                       accelerated=True)
+                       accelerated=True, device='cpu')
         assert_allclose(result, read_file('mlem_accel.npy'), rtol=1e-2)
 
     @unittest.skipUnless("CUDA_VERSION" in os.environ, "CUDA_VERSION not set.")
@@ -162,7 +162,7 @@ class ReconstructionAlgorithmTestCase(unittest.TestCase):
 
     def test_sirt_accel(self):
         result = recon(self.prj, self.ang, algorithm='sirt',
-                       num_iter=4, accelerated=True)
+                       num_iter=4, accelerated=True, device='cpu')
         assert_allclose(result, read_file('sirt_accel.npy'), rtol=1e-2)
 
     @unittest.skipUnless("CUDA_VERSION" in os.environ, "CUDA_VERSION not set.")
