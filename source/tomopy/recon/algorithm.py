1f8975299cd0367bd8d205bcea52a518e9a2a69b
Alan L Kastengren <akastengren@anl.gov>
2021-03-20 14:59:45 -0500

    Even more PEP8 formatting fixes

--- source/tomopy/recon/algorithm.py
+++ source/tomopy/recon/algorithm.py
@@ -394,9 +394,10 @@ def _dist_recon(tomo, center, recon, algorithm, args, kwargs, ncore, nchunk):
     # check if ncore is limited by env variable
     pythreads = os.environ.get("TOMOPY_PYTHON_THREADS")
     if pythreads is not None and ncore > int(pythreads):
-        logger.warning("Warning! 'TOMOPY_PYTHON_THREADS' has been set to '{0}',
-                        which is less than specified ncore={1}.
-                        Limiting ncore to {0}...".format(pythreads, ncore))
+        logger.warning(
+            "Warning! 'TOMOPY_PYTHON_THREADS' has been set to '{0}',
+            which is less than specified ncore={1}.
+            Limiting ncore to {0}...".format(pythreads, ncore))
         ncore = int(pythreads)
 
     logger.info("Reconstructing {} slice groups with {} master threads..."
