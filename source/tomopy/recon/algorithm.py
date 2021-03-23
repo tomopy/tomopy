ccf2cfa2df1aba8987e7a8827ac360d0b5534c55
Daniel Ching <carterbox@users.noreply.github.com>
2021-03-22 19:15:12 -0500

    DOC: Rephrase thread warning for clarity

--- source/tomopy/recon/algorithm.py
+++ source/tomopy/recon/algorithm.py
@@ -405,9 +405,9 @@ def _dist_recon(tomo, center, recon, algorithm, args, kwargs, ncore, nchunk):
     pythreads = os.environ.get("TOMOPY_PYTHON_THREADS")
     if pythreads is not None and ncore > int(pythreads):
         warnings.warn(
-            "Warning! 'TOMOPY_PYTHON_THREADS' has been set to '{0}',\
-            which is less than specified ncore={1}.\
-            Limiting ncore to {0}...".format(pythreads, ncore))
+            "The environment variable 'TOMOPY_PYTHON_THREADS' is limiting "
+            "the requested ncore={1} to {0} cores. "
+            "Set ncore <= 'TOMOPY_PYTHON_THREADS'.".format(pythreads, ncore))
         ncore = int(pythreads)
 
     logger.info("Reconstructing {} slice groups with {} master threads..."
