5acaf3fdd5c5856e86caa3e6cbda086eba2ddfe6
Daniil Kazantsev <dkazanc@hotmail.com>
2023-02-14 22:01:19 +0000

    adding new lines

--- test/test_tomopy/test_prep/test_stripe.py
+++ test/test_tomopy/test_prep/test_stripe.py
@@ -148,17 +148,18 @@ class StripeRemovalTestCase(unittest.TestCase):
 
     def test_stripe_detection(self):
         assert_allclose(
-            srm.stripes_detect3d(read_file('test_stripe_data.npy'), 
+            srm.stripes_detect3d(read_file('test_stripe_data.npy'),
                                  vert_filter_size_perc=25, 
                                  radius_size=1),
             read_file('stripes_detect3d.npy'), rtol=1e-6)
 
     def test_stripe_mask(self):
         assert_allclose(
-            srm.stripes_mask3d(read_file('stripes_detect3d.npy'), 
+            srm.stripes_mask3d(read_file('stripes_detect3d.npy'),
                                threshold=0.2,
                                stripe_length_perc=30.0,
                                stripe_depth_perc=0.0,
                                stripe_width_perc=20.0,
                                sensitivity_perc=80.0),
-            read_file('stripes_mask3d.npy'), rtol=1e-6)        
+            read_file('stripes_mask3d.npy'), rtol=1e-6)
+        
